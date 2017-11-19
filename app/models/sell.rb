f827533eaa90cd8345e559f2703c2623f4ce6b57
aline-aline <alinecris@gmail.com>
2017-11-19 12:33:20 -0200

    Add scaffolds for clients, suppliers, products, services, campaigns, discounts and sells

---
+++ app/models/sell.rb
@@ -0,0 +1,11 @@
+class Sell < ApplicationRecord
+  include Fae::BaseModelConcern
+
+  def fae_display_field
+    
+  end
+
+
+  belongs_to :discount
+  belongs_to :client
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
 
   namespace :admin do
+    resources :sells
+    resources :discounts
+    resources :campaigns
+    resources :services
+    resources :products
+    resources :suppliers
+    resources :clients
   end
   # mount Fae below your admin namespec
   mount Fae::Engine => '/admin'
