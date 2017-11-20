6b45970ee475becf362cdbaa28b16e074de576a0
aline-aline <alinecris@gmail.com>
2017-11-20 15:35:57 -0200

    Customize the behavior of all models

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,6 +1,9 @@
 class Discount < ApplicationRecord
   include Fae::BaseModelConcern
 
+  validates :title, uniqueness: true, presence: true
+  validates :value, presence: true
+
   def fae_display_field
     title
   end
