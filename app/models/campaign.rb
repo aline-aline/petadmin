057ec7475f68ced653273a0b5543fd9a573ab881
aline-aline <alinecris@gmail.com>
2017-11-20 18:06:49 -0200

    Update campaign job

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,12 +3,19 @@ class Campaign < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
+  has_many :campaign_clients
+  has_many :clients, through: :campaign_clients
 
-  has_many :campaign_clients, dependent: :destroy
-  has_many :clients, through: :campaign_clients 
+  after_save :schedule_emails
 
   def fae_display_field
     title
   end
 
+  def schedule_emails
+    Client.all.each do |client|
+      CampaignClient.create(campaign: self, client: client)
+      CampaingJob.perform_later client, self.title, self.body
+    end
+  end
 end
